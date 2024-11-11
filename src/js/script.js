b83051e7448fee3ac0b4075b4e7776e84ec1ad03
Olrosa <sayonet@mail.ru>
2024-11-11 17:27:30 +0300

    edits have been made

--- src/js/script.js
+++ src/js/script.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (document.querySelector('.services__slider')) {
         const servicesSlider = new Swiper('.services__slider', {
             slidesPerView: 3,
-            spaceBetween: 10,
+            spaceBetween: 20,
             scrollbar: {
                 el: '.services__scrollbar', // Уникальная полоса прокрутки
                 draggable: true,
@@ -39,7 +39,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (document.querySelector('.clients__slider')) {
         const clientsSlider = new Swiper('.clients__slider', {
             slidesPerView: 4,
-            spaceBetween: 10,
+            spaceBetween: 20,
             scrollbar: {
                 el: '.clients__scrollbar', // Уникальная полоса прокрутки
                 draggable: true,
@@ -55,4 +55,84 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
+    // Инициализация второго слайдера, если элемент существует
+    if (document.querySelector('.price__slider')) {
+        const priceSlider = new Swiper('.price__slider', {
+            slidesPerView: 2.5,
+            spaceBetween: 20,
+            scrollbar: {
+                el: '.price__scrollbar', // Уникальная полоса прокрутки
+                draggable: true,
+            },
+            mousewheel: {
+                forceToAxis: true,
+            },
+            breakpoints: {
+                320: { slidesPerView: 1, centeredSlides: true, initialSlide: 0 },
+                576: { slidesPerView: 2, centeredSlides: false, initialSlide: 0 },
+                1024: { slidesPerView: 2.5 },
+            },
+        });
+    }
+
+    // Инициализация второго слайдера, если элемент существует
+    if (document.querySelector('.docs__slider')) {
+        const docsSlider = new Swiper('.docs__slider', {
+            slidesPerView: 4,
+            spaceBetween: 20,
+            scrollbar: {
+                el: '.docs__scrollbar', // Уникальная полоса прокрутки
+                draggable: true,
+            },
+            mousewheel: {
+                forceToAxis: true,
+            },
+            breakpoints: {
+                320: { slidesPerView: 1, centeredSlides: true, initialSlide: 0 },
+                576: { slidesPerView: 2, centeredSlides: false, initialSlide: 0 },
+                1024: { slidesPerView: 4 },
+            },
+        });
+    }
+
+    // Инициализация второго слайдера, если элемент существует
+    if (document.querySelector('.letters__slider')) {
+        const lettersSlider = new Swiper('.letters__slider', {
+            slidesPerView: 4,
+            spaceBetween: 20,
+            scrollbar: {
+                el: '.letters__scrollbar', // Уникальная полоса прокрутки
+                draggable: true,
+            },
+            mousewheel: {
+                forceToAxis: true,
+            },
+            breakpoints: {
+                320: { slidesPerView: 1, centeredSlides: true, initialSlide: 0 },
+                576: { slidesPerView: 2, centeredSlides: false, initialSlide: 0 },
+                1024: { slidesPerView: 4 },
+            },
+        });
+    }
+
+    // Инициализация второго слайдера, если элемент существует
+    if (document.querySelector('.live__slider')) {
+        const liveSlider = new Swiper('.live__slider', {
+            slidesPerView: 4,
+            spaceBetween: 20,
+            scrollbar: {
+                el: '.live__scrollbar', // Уникальная полоса прокрутки
+                draggable: true,
+            },
+            mousewheel: {
+                forceToAxis: true,
+            },
+            breakpoints: {
+                320: { slidesPerView: 1, centeredSlides: true, initialSlide: 0 },
+                576: { slidesPerView: 2, centeredSlides: false, initialSlide: 0 },
+                1024: { slidesPerView: 4 },
+            },
+        });
+    }
+    
 });
